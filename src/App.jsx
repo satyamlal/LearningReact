b5794c29ddd93f1b26039b32125da9c2726c2c45
Satyam Lal <satyamlal@users.noreply.github.com>
2025-06-29 18:09:35 +0530

    Cards now have dynamic datas

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,9 @@ import Card from "./Components/Card";
 
 function App() {
   return (
-    <div>
+    <>
       <Card />
-    </div>
+    </>
   );
 }
 
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,28 +1,49 @@
 import React from "react";
 
 const Card = () => {
-  const data = {
-    image:
-      "https://images.unsplash.com/photo-1633174524778-61a18ee54490?q=80&w=1196&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    title: "Amazon Basics.",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate esse architecto unde!",
-  };
+  const data = [
+    {
+      image:
+        "https://images.unsplash.com/photo-1633174524778-61a18ee54490?q=80&w=1196&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      title: "Amazon Basics.",
+      desc: "Amazon has launched their new factory to build and compete with other marnufacturers in the market.",
+    },
+    {
+      image:
+        "https://images.unsplash.com/photo-1621330396173-e41b1cafd17f?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      title: "Android Phones",
+      desc: "New Android Phone with highly customizable specifications are going to be launched this September.",
+    },
+    {
+      image:
+        "https://images.unsplash.com/photo-1710905219584-8521769e3678?q=80&w=1255&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      title: "Apple Products",
+      desc: "Apple is planning to launch their new AI oriented devices later this year in December 2025.",
+    },
+  ];
 
   return (
     <div className="w-full h-screen flex items-center justify-center gap-10 bg-zinc-200">
-      <div className="w-52 bg-zinc-100 rounded-lg overflow-hidden">
-        <div className="w-full h-32 bg-zinc-300 ">
-          <img
-            className="w-full h-full object-cover"
-            src={data.image}
-            alt="Amazon Box"
-          />
-        </div>
-        <div className="w-full px-3 py-4">
-          <h2 className="font-semibold">{data.title}</h2>
-          <p className="text-xs mt-3">{data.desc}</p>
-        </div>
-      </div>
+      {data.map((elem, index) => {
+        return (
+          <div
+            key={index}
+            className="w-52 bg-zinc-100 rounded-lg overflow-hidden"
+          >
+            <div className="w-full h-32 bg-zinc-300 ">
+              <img
+                className="w-full h-full object-cover"
+                src={elem.image}
+                alt="Amazon Box"
+              />
+            </div>
+            <div className="w-full px-3 py-4">
+              <h2 className="font-semibold">{elem.title}</h2>
+              <p className="text-xs mt-3">{elem.desc}</p>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
