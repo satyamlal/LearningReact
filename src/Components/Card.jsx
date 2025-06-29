58329039c19413c645c24fb4b0fc6c9bde327256
Satyam Lal <satyamlal@users.noreply.github.com>
2025-06-29 21:37:25 +0530

    Conditional Rendering working properly for cards

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -7,18 +7,21 @@ const Card = () => {
         "https://images.unsplash.com/photo-1633174524778-61a18ee54490?q=80&w=1196&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       title: "Amazon Basics.",
       desc: "Amazon has launched their new factory to build and compete with other marnufacturers in the market.",
+      inStock: true,
     },
     {
       image:
         "https://images.unsplash.com/photo-1621330396173-e41b1cafd17f?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       title: "Android Phones",
-      desc: "New Android Phone with highly customizable specifications are going to be launched this September.",
+      desc: "Xiaomi has plans to launch their new highly customizable android phones on their new event on September 2025.",
+      inStock: false,
     },
     {
       image:
         "https://images.unsplash.com/photo-1710905219584-8521769e3678?q=80&w=1255&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       title: "Apple Products",
       desc: "Apple is planning to launch their new AI oriented devices later this year in December 2025.",
+      inStock: false,
     },
   ];
 
@@ -40,6 +43,13 @@ const Card = () => {
             <div className="w-full px-3 py-4">
               <h2 className="font-semibold">{elem.title}</h2>
               <p className="text-xs mt-3">{elem.desc}</p>
+              <button
+                className={`px-2 cursor-pointer py-1 mt-3 text-white rounded-lg ${
+                  elem.inStock ? "bg-red-400" : "bg-gray-400"
+                }`}
+              >
+                {elem.inStock ? "Buy Now!" : "Know More!"}
+              </button>
             </div>
           </div>
         );
